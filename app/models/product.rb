0d8cddfb87365d9b6d547a43d70da0585b5cd8d2
+ <+>
2023-09-20 00:39:30 -0400

    built the relationships between the models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    belongs_to :user, dependent: :destroy
+    has_many :reviews, dependent: :destroy
+    has_many :users_who_reviewed, through: :reviews, source: :user
 end
