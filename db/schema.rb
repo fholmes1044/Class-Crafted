e6536610da16c2c27284d16288b4ff531253cf6c
+ <+>
2023-09-22 15:45:58 -0400

    added a seller_id column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_032543) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_194212) do
   create_table "products", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_032543) do
     t.integer "inventory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seller_id"
   end
 
   create_table "reviews", force: :cascade do |t|
